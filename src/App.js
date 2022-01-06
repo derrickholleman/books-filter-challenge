ce862b081ec62718dc45480629ef309b3b361559
Derrick Holleman <derrickholleman@gmail.com>
2022-01-05 21:03:21 -0600

    filter works

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Book from "./Book";
 import SearchForm from "./SearchForm";
 const booksData = require("./books.json");
 
 function App() {
+  const [books, setBooks] = useState(booksData);
+  const [isFiltering, setIsFiltering] = useState(false);
+  const [authorSearchText, setAuthorSearchText] = useState("");
+  const [titleSearchText, setTitleSearchText] = useState("");
+  const [countrySearchText, setCountrySearchText] = useState("");
+  const [languageSearchText, setLanguageSearchText] = useState("");
+  const [yearSearchText, setYearSearchText] = useState("");
+  const [filteredBooks, setFilteredBooks] = useState([]);
+
   return (
     <div className="App">
-      <SearchForm />
+      <SearchForm
+        setIsFiltering={setIsFiltering}
+        authorSearchText={authorSearchText}
+        setAuthorSearchText={setAuthorSearchText}
+        setFilteredBooks={setFilteredBooks}
+        titleSearchText={titleSearchText}
+        setTitleSearchText={setTitleSearchText}
+        countrySearchText={countrySearchText}
+        setCountrySearchText={setCountrySearchText}
+        languageSearchText={languageSearchText}
+        setLanguageSearchText={setLanguageSearchText}
+        yearSearchText={yearSearchText}
+        setYearSearchText={setYearSearchText}
+        books={booksData}
+      />
 
       <div className="books">
-        {booksData.map((book, index) => (
+        {!isFiltering &&
+          books.map((book, index) => <Book key={index} book={book} />)}
+
+        {filteredBooks.map((book, index) => (
           <Book key={index} book={book} />
         ))}
       </div>
--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,27 +1,120 @@
-import React from "react";
+import { useEffect } from "react";
 
-const SearchForm = () => {
+const SearchForm = ({
+  setIsFiltering,
+  authorSearchText,
+  setAuthorSearchText,
+  titleSearchText,
+  setTitleSearchText,
+  countrySearchText,
+  setCountrySearchText,
+  languageSearchText,
+  setLanguageSearchText,
+  yearSearchText,
+  setYearSearchText,
+  setFilteredBooks,
+  books,
+}) => {
+  const handleAuthorChange = (e) => {
+    setAuthorSearchText(e.target.value);
+  };
+
+  const handleTitleChange = (e) => {
+    setTitleSearchText(e.target.value);
+  };
+
+  const handleCountryChange = (e) => {
+    setCountrySearchText(e.target.value);
+  };
+
+  const handleLanguageChange = (e) => {
+    setLanguageSearchText(e.target.value);
+  };
+
+  const handleYearChange = (e) => {
+    setYearSearchText(e.target.value);
+  };
+
+  useEffect(() => {
+    if (
+      !authorSearchText &&
+      !titleSearchText &&
+      !countrySearchText &&
+      !languageSearchText &&
+      !yearSearchText
+    ) {
+      setIsFiltering(false);
+    } else {
+      setIsFiltering(true);
+    }
+
+    setFilteredBooks(
+      books.filter((book) => {
+        return (
+          book.author
+            .toLowerCase()
+            .includes(authorSearchText.toLowerCase().trim()) &&
+          book.title
+            .toLowerCase()
+            .includes(titleSearchText.toLowerCase().trim()) &&
+          book.country
+            .toLowerCase()
+            .includes(countrySearchText.toLowerCase().trim()) &&
+          book.language
+            .toLowerCase()
+            .includes(languageSearchText.toLowerCase().trim()) &&
+          book.year.toString().includes(yearSearchText.trim())
+        );
+      })
+    );
+  }, [
+    books,
+    authorSearchText,
+    titleSearchText,
+    countrySearchText,
+    languageSearchText,
+    yearSearchText,
+  ]);
+
+  console.log(authorSearchText);
+
   return (
     <div className="search-form">
       <div>
         <label htmlFor="author">author</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={authorSearchText}
+          onChange={handleAuthorChange}
+        />
       </div>
       <div>
         <label htmlFor="title">title</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={titleSearchText}
+          onChange={handleTitleChange}
+        />
       </div>
       <div>
         <label htmlFor="country">country</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={countrySearchText}
+          onChange={handleCountryChange}
+        />
       </div>
       <div>
         <label htmlFor="language">language</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={languageSearchText}
+          onChange={handleLanguageChange}
+        />
       </div>
       <div>
         <label htmlFor="year">year</label>
-        <input type="text" />
+        <input type="text" value={yearSearchText} onChange={handleYearChange} />
       </div>
     </div>
   );
